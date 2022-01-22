a2dbcb638685ec2fbfdc121fa015ccf2b21933cf
Xavier Campdepadros Ortigosa <bullt1980@outlook.es>
2022-01-22 08:48:54 +0100

    Init

---
+++ TestListView/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls;
+
+namespace TestListView
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Users = new ObservableCollection<User>
+            {
+                new User {Name = "Xavi"},
+                new User {Name = "Noa"},
+                new User {Name = "Jenny"}
+            };
+
+            foreach (User userSelected in Users)
+            {
+                UsersListView.SelectedItems.Add(userSelected);
+            }
+        }
+
+        public ObservableCollection<User> Users { get; set; }
+    }
+}
---
+++ TestListView/User.cs
@@ -0,0 +1,7 @@
+﻿namespace TestListView
+{
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
